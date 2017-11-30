6fc56c6e4e6ee0b7a9560ce9f62d66f490280972
TingliangZhang <32335780+TingliangZhang@users.noreply.github.com>
2017-11-30 21:20:37 +0800

    20通道PWM输出DEBUG

--- 42-STM32F429_SERVO/User/main.c
+++ 42-STM32F429_SERVO/User/main.c
@@ -27,6 +27,9 @@ int main(void) {
 	/* Initialize delay */
 	TM_DELAY_Init();
 	
+	//PD15 PA1 PA2 PA8 PA9 PC8  PA10 PC6 PC7 PC8     ≤ªƒ‹”√
+	
+	
 	/* Initialize servo 1, TIM2, Channel 1, Pinspack 2 = PA5 */
 	TM_SERVO_Init(&Servo1, TIM2, TM_PWM_Channel_1, TM_PWM_PinsPack_2);
 	
@@ -36,29 +39,37 @@ int main(void) {
 	/* Initialize servo 3, TIM2, Channel 1, Pinspack 1 = PA0 */
 	TM_SERVO_Init(&Servo3, TIM2, TM_PWM_Channel_1, TM_PWM_PinsPack_1);	
 	
-	/* Initialize servo 4, TIM2, Channel 2, Pinspack 1 = PA1 */
-	TM_SERVO_Init(&Servo4, TIM2, TM_PWM_Channel_2, TM_PWM_PinsPack_1);
+	
+	
+	/* Initialize servo 4, TIM4, Channel 2, Pinspack 1 = PD13 */
+	TM_SERVO_Init(&Servo4, TIM4, TM_PWM_Channel_2, TM_PWM_PinsPack_2);
 		
-	/* Initialize servo 5, TIM2, Channel 3, Pinspack 1 = PA2 */
-	TM_SERVO_Init(&Servo5, TIM2, TM_PWM_Channel_3, TM_PWM_PinsPack_1);
+	/* Initialize servo 5, TIM4, Channel 3, Pinspack 1 = PB0 */
+	TM_SERVO_Init(&Servo5, TIM3, TM_PWM_Channel_3, TM_PWM_PinsPack_1);
+		
+		
 		
 	/* Initialize servo 6, TIM2, Channel 4, Pinspack 1 = PA3 */
 	TM_SERVO_Init(&Servo6, TIM2, TM_PWM_Channel_4, TM_PWM_PinsPack_1);
 		
 	/* Initialize servo 7, TIM3, Channel 1, Pinspack 1 = PA6 */
-	TM_SERVO_Init(&Servo7, TIM3, TM_PWM_Channel_1, TM_PWM_PinsPack_2);
+	TM_SERVO_Init(&Servo7, TIM3, TM_PWM_Channel_1, TM_PWM_PinsPack_1);
 		
 	/* Initialize servo 8, TIM3, Channel 2, Pinspack 2 = PA7 */
 	TM_SERVO_Init(&Servo8, TIM3, TM_PWM_Channel_2, TM_PWM_PinsPack_1);
 		
-	/* Initialize servo 9, TIM1, Channel 1, Pinspack 2 = PA8 */
-	TM_SERVO_Init(&Servo9, TIM1, TM_PWM_Channel_1, TM_PWM_PinsPack_1);
 		
-	/* Initialize servo 10, TIM1, Channel 2, Pinspack 2 = PA9 */
-	TM_SERVO_Init(&Servo10, TIM1, TM_PWM_Channel_2, TM_PWM_PinsPack_1);
 		
-	/* Initialize servo 11, TIM1, Channel 3, Pinspack 2 = PA10 */
-	TM_SERVO_Init(&Servo11, TIM1, TM_PWM_Channel_3, TM_PWM_PinsPack_1);
+	/* Initialize servo 9, TIM1, Channel 1, Pinspack 2 = PD12 */
+	TM_SERVO_Init(&Servo9, TIM4, TM_PWM_Channel_1, TM_PWM_PinsPack_1);
+		
+	/* Initialize servo 10, TIM1, Channel 2, Pinspack 2 = PD15 */
+	TM_SERVO_Init(&Servo10, TIM4, TM_PWM_Channel_4, TM_PWM_PinsPack_1);
+		
+		
+		
+	/* Initialize servo 11, TIM1, Channel 3, Pinspack 2 = PF6 */
+	TM_SERVO_Init(&Servo11, TIM10, TM_PWM_Channel_1, TM_PWM_PinsPack_2);
 		
 	/* Initialize servo 12, TIM1, Channel 4, Pinspack 2 = PA11 */
 	TM_SERVO_Init(&Servo12, TIM1, TM_PWM_Channel_4, TM_PWM_PinsPack_1);	
@@ -75,16 +86,16 @@ int main(void) {
 	/* Initialize servo 16, TIM4, Channel 4, Pinspack 1 = PB9 */
 	TM_SERVO_Init(&Servo16, TIM4, TM_PWM_Channel_4, TM_PWM_PinsPack_1);
 		
-	/* Initialize servo 17, TIM8, Channel 1, Pinspack 1 = PA5 */
-	TM_SERVO_Init(&Servo17, TIM8, TM_PWM_Channel_1, TM_PWM_PinsPack_1);
+	/* Initialize servo 17, TIM8, Channel 1, Pinspack 1 = PF7 */
+	TM_SERVO_Init(&Servo17, TIM11, TM_PWM_Channel_1, TM_PWM_PinsPack_2);
 		
-	/* Initialize servo 18, TIM8, Channel 2, Pinspack 1 = PA5 */
-	TM_SERVO_Init(&Servo18, TIM8, TM_PWM_Channel_2, TM_PWM_PinsPack_1);
+	/* Initialize servo 18, TIM8, Channel 2, Pinspack 1 = PF8 */
+	TM_SERVO_Init(&Servo18, TIM13, TM_PWM_Channel_1, TM_PWM_PinsPack_2);
 		
-	/* Initialize servo 19, TIM8, Channel 3, Pinspack 1 = PA5 */
-	TM_SERVO_Init(&Servo19, TIM8, TM_PWM_Channel_3, TM_PWM_PinsPack_1);
+	/* Initialize servo 19, TIM8, Channel 3, Pinspack 1 = PF9 */
+	TM_SERVO_Init(&Servo19, TIM14, TM_PWM_Channel_1, TM_PWM_PinsPack_2);
 		
-	/* Initialize servo 20, TIM8, Channel 4, Pinspack 1 = PA5 */
+	/* Initialize servo 20, TIM8, Channel 4, Pinspack 1 = PC9 */
 	TM_SERVO_Init(&Servo20, TIM8, TM_PWM_Channel_4, TM_PWM_PinsPack_1);
 
 	
