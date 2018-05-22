ae26eb805f1b5c39feb9b5185004864f56f10a9b
Tingliang Zhang <32335780+TingliangZhang@users.noreply.github.com>
2018-05-22 23:03:59 +0800

    v2 program

---
+++ agan_v2.1/agan_v2.1.ino
@@ -0,0 +1,30 @@
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  int delta=random(0,100);   //random num from 0 to 100\(long int)
+  float Bias=delta/100;
+  
+
+
+  
+  for (pos = 30; pos <= 150; pos += 5) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(3);                       // waits 15ms for the servo to reach the position
+
+  }
+  for (pos = 150; pos >= 30; pos -= 5) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(3);                       // waits 15ms for the servo to reach the position
+  }
+}
